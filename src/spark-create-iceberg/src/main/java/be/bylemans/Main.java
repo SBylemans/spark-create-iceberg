4314dc1b632591fcb6f2200b6a4745169bddc1b3
Sander Bylemans <sbylemans@gmail.com>
2025-11-04 11:08:41 +0100

    Fix docker

--- src/spark-create-iceberg/src/main/java/be/bylemans/Main.java
+++ src/spark-create-iceberg/src/main/java/be/bylemans/Main.java
@@ -1,8 +1,13 @@
 package be.bylemans;
 
+import static org.apache.hadoop.mapreduce.lib.output.committer.manifest.files.EntryStatus.dir;
+
 import java.io.File;
 import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
 import java.util.Objects;
+import java.util.stream.Stream;
 import org.apache.commons.io.FileUtils;
 import org.apache.spark.sql.Dataset;
 import org.apache.spark.sql.Row;
@@ -10,10 +15,17 @@ import org.apache.spark.sql.SparkSession;
 import org.apache.spark.sql.types.StructType;
 import org.eclipse.jgit.api.Git;
 import org.eclipse.jgit.api.errors.GitAPIException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class Main {
 
+
   public static void main(String[] args) throws Exception {
+    Logger logger = LoggerFactory.getLogger(Main.class);
+
+    logger.info("Getting schema from {} - {}", args[1], args[2]);
+
     SparkSession spark = SparkSession.builder()
         .appName("YAML Schema Reader")
         .getOrCreate();
@@ -21,6 +33,14 @@ public class Main {
         .setURI(args[1])
         .setDirectory(new File("/tmp/data"))
         .call()) {
+
+      try (Stream<Path> stream = Files.walk(Path.of("/tmp/data"))) {
+        stream.filter(Files::isRegularFile)
+            .forEach(f -> logger.info("Found file: {}", f));
+      } catch (IOException e) {
+        e.printStackTrace();
+      }
+
       Schema schema = Schema.fromYaml("/tmp/data/%s".formatted(args[2]));
       StructType sparkSchema = Objects.requireNonNull(schema).toSparkSchema();
       Dataset<Row> dataFrame = spark.createDataFrame(spark.emptyDataFrame().javaRDD(), sparkSchema);
